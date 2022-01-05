57b31a2c62189f3635dafd103d79e0db6271d6a9
WonChul Park <pwcasdf@gmail.com>
2022-01-05 18:44:45 +0900

    stdout added    @pwcasdf

--- app.py
+++ app.py
@@ -6,23 +6,37 @@ import sys
 app = Flask(__name__)
 
 # set defualt logger level
-#logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # logger object creation and set logging level, debug >> info >> warning >> error >> critical
 testLogger = logging.getLogger('not_default')
-#streamHandler = logging.StreamHandler(sys.stdout)
-#testLogger.addHandler(streamHandler)
+streamHandler = logging.StreamHandler(sys.stdout)
+testLogger.addHandler(streamHandler)
 testLogger.setLevel(logging.INFO)
+
+testLogger2 = logging.getLogger('not_default2')
+streamHandler2 = logging.StreamHandler(sys.stderr)
+testLogger2.addHandler(streamHandler2)
+testLogger2.setLevel(logging.INFO)
+
 
 @app.route('/')
 def hello_world():
-    print(datetime.datetime.now(), 'this is print@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    app.logger.info('this is app.logger.info^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
-    logging.info('this is info!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    logging.warning('this is warning#################################')
-    logging.debug('this is debug1231231231')
+    print('\n\n--------------------------------------------------------------\n\n')
+    print(datetime.datetime.now(), 'print')
+    
+    app.logger.info('info app.logger')
+    
+    logging.debug('debug default')
+    logging.info('info default')
+    logging.warning('warning default')
 
-    testLogger.info('this is info testLogger!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    testLogger.warning('this is warning testLogger#################################')
-    testLogger.debug('this is debug testlogger 123413414212414142124')
+    testLogger.debug('debug testLogger')
+    testLogger.info('info testLogger')
+    testLogger.warning('warning testLogger')
+
+    testLogger2.debug('debug testLogger2')
+    testLogger2.info('info testLogger2')
+    testLogger2.warning('warning testLogger2')
+    
     return str(datetime.datetime.now())
